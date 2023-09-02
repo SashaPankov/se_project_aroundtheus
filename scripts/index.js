d84f75769e11ab0090c242a5fef8e4ad94036119
SashaPankov <alexvp70@gmail.com>
2023-09-03 00:22:25 +0300

    tasks of project 5 completed

--- scripts/index.js
+++ scripts/index.js
@@ -26,7 +26,6 @@ const initialCards = [
 ];
 
 // Elements
-
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileCloseButton = document.querySelector("#profile-close-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
@@ -40,18 +39,44 @@ const profileEditForm = document.forms["profile-edit-form"];
 const cardTemplate = document.querySelector("#card-template").content;
 const cardsList = document.querySelector(".cards__list");
 
+const cardAddButton = document.querySelector("#card-add-button");
+const cardCloseButton = document.querySelector("#card-close-button");
+const cardAddModal = document.querySelector("#card-add-modal");
+const cardTitleInput = document.querySelector("#card-title-input");
+const cardImageURLInput = document.querySelector("#card-imageURL-input");
+const cardAddForm = document.forms["card-add-form"];
+
+const cardImageModal = document.querySelector("#image-view-modal");
+const modalImage = document.querySelector(".modal__image");
+const imageCloseButton = document.querySelector("#image-close-button");
+const modalCardTitle = document.querySelector(".modal__card-title");
+
 // Functions
-function closePopup() {
-  profileEditModal.classList.remove("modal_opened");
+function closePopup(evt) {
+  if (cardImageModal.classList.contains("modal_opened")) {
+    cardImageModal.classList.remove("modal_opened");
+    return;
+  }
+
+  (profileEditModal.classList.contains("modal_opened")
+    ? profileEditModal
+    : cardAddModal
+  ).classList.remove("modal_opened");
 }
 
 function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImageElement = cardElement.querySelector(".card__image");
   const cardTitleElement = cardElement.querySelector(".card__title");
+  const cardLikeElement = cardElement.querySelector(".card__like-button");
+  const cardDeleteElement = cardElement.querySelector(".card__delete-button");
+
   cardImageElement.src = cardData.link;
   cardImageElement.alt = cardData.name;
   cardTitleElement.textContent = cardData.name;
+  cardLikeElement.addEventListener("click", changeLike);
+  cardDeleteElement.addEventListener("click", deleteCard);
+  cardImageElement.addEventListener("click", showImageModal);
   return cardElement;
 }
 
@@ -64,6 +89,34 @@ function handleProfileEditModalSubmit(evt) {
   closePopup();
 }
 
+function handleCardAddModalSubmit(evt) {
+  evt.preventDefault();
+  const newCard = { name: cardTitleInput.value, link: cardImageURLInput.value };
+
+  cardsList.prepend(getCardElement(newCard));
+  closePopup();
+}
+
+function changeLike(evt) {
+  evt.target.style.backgroundImage = evt.target.style.backgroundImage.includes(
+    "inactive"
+  )
+    ? "url('../images/like-active.svg')"
+    : "url('../images/like-inactive.svg')";
+}
+
+function deleteCard(evt) {
+  evt.target.parentElement.remove();
+}
+
+function showImageModal(evt) {
+  modalImage.src = evt.target.src;
+  modalImage.alt = evt.target.alt;
+  modalCardTitle.textContent = evt.target.alt;
+
+  cardImageModal.classList.add("modal_opened");
+}
+
 // Event Listeners
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
@@ -72,11 +125,19 @@ profileEditButton.addEventListener("click", () => {
 });
 
 profileCloseButton.addEventListener("click", closePopup);
-
 profileEditForm.addEventListener("submit", handleProfileEditModalSubmit);
 
-// cards filling
+cardAddButton.addEventListener("click", () => {
+  cardTitleInput.value = "Title";
+  cardImageURLInput.value = "Image URL";
+  cardAddModal.classList.add("modal_opened");
+});
+cardCloseButton.addEventListener("click", closePopup);
+cardAddForm.addEventListener("submit", handleCardAddModalSubmit);
 
+imageCloseButton.addEventListener("click", closePopup);
+
+// cards filling
 initialCards.forEach((cardData) => {
   cardsList.append(getCardElement(cardData));
 });
